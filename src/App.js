b5e55b8b054065b8d878635abadcffb054516478
paskeviciute.rima@gmail.com <paskeviciute.eima@gmail.com>
2020-03-13 13:50:09 +0000

    add movie form component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MovieList from './MovieList.js';
 import Nav from './Nav';
 import { MovieProvider } from './MovieContext';
+import AddMovie from './AddMovie';
 
 function App() {
   return (
     <MovieProvider>
     <div className="App">
       <Nav />
+      <AddMovie />
       <MovieList />
     </div>
     </MovieProvider>
